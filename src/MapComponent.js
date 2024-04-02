f050fb084678c965fe6a59f79aad425ca2a748b4
dingxu <dahuaidanxu@aliyun.com>
2024-04-02 21:54:16 +0800

    ✨ feat: 显示病理图

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -2,32 +2,101 @@ import React, { useEffect } from 'react';
 import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import { TileWMS } from 'ol/source';
+import { Zoomify } from 'ol/source';
+import path from 'path-browserify';
 
 const MapComponent = () => {
   useEffect(() => {
     // GeoServer WMS URL
-    const geoserverWMSUrl = '/geoserver/test/wms';
 
+    const sid = "65d1a535a07b7e1000e52c04";
+    const token = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTcxMjA2MjgzOSwiZXhwIjoxNzE0NjU0ODM5fQ.eyJpZCI6IjY1Njk5MjQ0YWY4YmU1ODU3ODVkMWNhOSJ9.QzoLXHu8n62-R-R2OPE5Ax6sW3hVoU7lRus99CGpAetJhAo7HIbHKSbkjmVxTaviYQaribDz2-Ptfny-YO-swg";
+    const imageWeight = 91136;
+    const imageHeight = 68096;
+    const baseurl = '/hg/';
+    const tokenHeaders = new Headers();// HTTP 请求头
+    tokenHeaders.append('Authorization', "Token " + token);
+
+    const getUrl = (sid) => {
+      // 判断 Zoomify 数据源是否可用，如果不可用则使用普通图片路径
+      var url = '/hg/image/' + sid + '/{TileGroup}/{z}/{x}_{y}.jpeg';
+      let r = new XMLHttpRequest();
+      r.open('GET', '/hg/image/' + sid + '/8/0_0.jpeg', false)
+      r.send(null);
+      return url;
+    }
+
+    const zoomifySource = new Zoomify({
+      url: sid ? getUrl(sid) : '',
+      crossOrigin: 'anonymous',
+      size: [imageWeight, imageHeight],
+      cacheSize: 2000
+    });
+
+    zoomifySource.setTileLoadFunction(async function (imageTile, src) {
+      src = path.resolve(src.replace(/TileGroup\d+/, ''));
+      let _srcArr = src.split('/');
+      let z = _srcArr[_srcArr.length - 2];
+      _srcArr[_srcArr.length - 2] = parseInt(z) + 8;
+      src = _srcArr.join('/');
+
+      try {
+        const blob = await getImg(src);
+        var objectURL = URL.createObjectURL(blob);
+        imageTile.getImage().src = objectURL;
+      } catch (error) {
+        console.log(error)
+      }
+    })
+
+    // 获取图片数据
+    const getImg = (url) => {
+      return tFetch(url).then(checkStatus).then(parseBLOB);
+    }
+
+    const checkStatus = (response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response
+      } else if (response.status == 401) {
+
+      } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    }
+
+    const parseBLOB = (response) => {
+      return response.blob();
+    }
+
+    // 封装 fetch 请求，添加授权头
+    const tFetch = (url, method = 'GET') => {
+      var prefixedUrl = url.startsWith(baseurl) ? url : baseurl + url;
+      var myRequest = new Request(prefixedUrl, {
+        headers: tokenHeaders,
+        method,
+      });
+      return fetch(myRequest)
+    }
     // OpenLayers Map
     const map = new Map({
       target: 'map',
       layers: [
         new TileLayer({
-          source: new TileWMS({
-            url: geoserverWMSUrl,
-            params: { 'LAYERS': 'test:Philips1', 'TILED': true },
-            serverType: 'geoserver',
-            crossOrigin: 'anonymous',
-          }),
+          source: zoomifySource,
         }),
       ],
       view: new View({
-        center: [20204.856031830244, 14223.65701339989],
-        zoom:12,
+        center: [imageWeight / 2, -imageHeight / 2],
+        zoom: 11,
+        maxZoom: 16,
+        minZoom: 10
       }),
+
+      // controls: [],
     });
-    
+
     return () => {
       map.dispose();
     };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
   
 module.exports = function(app) {  
   app.use(  
-    '/geoserver', // 匹配所有以 /api 开头的请求路径  
+    '/hg', // 匹配所有以 /api 开头的请求路径  
     createProxyMiddleware({  
-      target: 'http://192.168.0.57:8080/geoserver', // 将请求代理到 http://localhost:5000  
+      target: 'http://101.33.116.111:7077/hg', // 将请求代理到 http://localhost:5000  
       changeOrigin: true, // 如果需要跨域，设置为true  
     })  
   );  
